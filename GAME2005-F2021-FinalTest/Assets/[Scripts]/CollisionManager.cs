22c5f9bea6b88f4dbd91e42b17cf27b5c078bb51
Russell Li <russell.li@rocketmail.com>
2021-12-17 14:26:28 -0500

    Completed the DummyBehaviour and the cube-bullet
    Completed the DummyBehaviour and the cube-bullet

--- GAME2005-F2021-FinalTest/Assets/[Scripts]/CollisionManager.cs
+++ GAME2005-F2021-FinalTest/Assets/[Scripts]/CollisionManager.cs
@@ -152,6 +152,53 @@ public class CollisionManager : MonoBehaviour
     }
 
 
+    public static void CheckCubeBulletDummy(BulletBehaviour s, DummyBehaviour b)
+    {
+        // get box closest point to sphere center by clamping
+        var x = Mathf.Max(b.min.x, Mathf.Min(s.transform.position.x, b.max.x));
+        var y = Mathf.Max(b.min.y, Mathf.Min(s.transform.position.y, b.max.y));
+        var z = Mathf.Max(b.min.z, Mathf.Min(s.transform.position.z, b.max.z));
+
+        var distance = Math.Sqrt((x - s.transform.position.x) * (x - s.transform.position.x) +
+                                 (y - s.transform.position.y) * (y - s.transform.position.y) +
+                                 (z - s.transform.position.z) * (z - s.transform.position.z));
+
+        if ((distance < s.radius) && (!s.isColliding))
+        {
+            // determine the distances between the contact extents
+            float[] distances = {
+                (b.max.x - s.transform.position.x),
+                (s.transform.position.x - b.min.x),
+                (b.max.y - s.transform.position.y),
+                (s.transform.position.y - b.min.y),
+                (b.max.z - s.transform.position.z),
+                (s.transform.position.z - b.min.z)
+            };
+
+            float penetration = float.MaxValue;
+            Vector3 face = Vector3.zero;
+
+            // check each face to see if it is the one that connected
+            for (int i = 0; i < 6; i++)
+            {
+                if (distances[i] < penetration)
+                {
+                    // determine the penetration distance
+                    penetration = distances[i];
+                    face = faces[i];
+                }
+            }
+
+            s.penetration = penetration;
+            s.collisionNormal = face;
+            //s.isColliding = true;
+
+
+            Reflect(s);
+        }
+
+    }
+
 
     // This helper function reflects the bullet when it hits an AABB face
     private static void Reflect(BulletBehaviour s)
@@ -169,7 +216,6 @@ public class CollisionManager : MonoBehaviour
             s.direction = new Vector3(s.direction.x, -s.direction.y, s.direction.z);
         }
     }
-
 
     public static void CheckAABBs(CubeBehaviour a, CubeBehaviour b)
     {
--- GAME2005-F2021-FinalTest/Assets/[Scripts]/DummyBehaviour.cs
+++ GAME2005-F2021-FinalTest/Assets/[Scripts]/DummyBehaviour.cs
@@ -1,18 +1,97 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Color = UnityEngine.Color;
 
+[System.Serializable]
+public class Contact2 : IEquatable<Contact2>
+{
+    public DummyBehaviour cube;
+    public Vector3 face;
+    public float penetration;
+
+    public Contact2(DummyBehaviour cube)
+    {
+        this.cube = cube;
+        face = Vector3.zero;
+        penetration = 0.0f;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null) return false;
+        Contact2 objAsContact = obj as Contact2;
+        if (objAsContact == null) return false;
+        else return Equals(objAsContact);
+    }
+
+    public override int GetHashCode()
+    {
+        return this.cube.gameObject.GetInstanceID();
+    }
+
+    public bool Equals(Contact2 other)
+    {
+        if (other == null) return false;
+
+        return (
+            (this.cube.gameObject.name.Equals(other.cube.gameObject.name)) &&
+            (this.face == other.face) &&
+            (Mathf.Approximately(this.penetration, other.penetration))
+            );
+    }
+
+    public override string ToString()
+    {
+        return "Cube Name: " + cube.gameObject.name + " face: " + face.ToString() + " penetration: " + penetration;
+    }
+}
+
+
+[System.Serializable]
 public class DummyBehaviour : MonoBehaviour
 {
+    [Header("Cube Attributes")]
+    public Vector3 size;
+    public Vector3 max;
+    public Vector3 min;
+    public bool isColliding;
+    public bool debug;
+    public List<Contact2> contacts;
+
+    private MeshFilter meshFilter;
+    public Bounds bounds;
+    public bool isGrounded;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        debug = false;
+        meshFilter = GetComponent<MeshFilter>();
+
+        bounds = meshFilter.mesh.bounds;
+        size = bounds.size;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        max = Vector3.Scale(bounds.max, transform.localScale) + transform.position;
+        min = Vector3.Scale(bounds.min, transform.localScale) + transform.position;
+
     }
+
+    private void OnDrawGizmos()
+    {
+        if (debug)
+        {
+            Gizmos.color = Color.magenta;
+
+            Gizmos.DrawWireCube(transform.position, Vector3.Scale(new Vector3(1.0f, 1.0f, 1.0f), transform.localScale));
+        }
+    }
+
 }
