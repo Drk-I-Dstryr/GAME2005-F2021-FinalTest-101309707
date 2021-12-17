8cd5f54688addbdaa5de4d48a40fff6b6eac30a6
Russell Li <russell.li@rocketmail.com>
2021-12-17 10:34:39 -0500

    Added name and student number labels to Start menu UI
    Added name and student number labels to Start menu UI

--- GAME2005-F2021-FinalTest/Assets/GoToMainScene.cs
+++ GAME2005-F2021-FinalTest/Assets/GoToMainScene.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GoToMainScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadMainLevel()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(1);
     }
 }
